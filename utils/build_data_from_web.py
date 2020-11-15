e9b8a3f5283b27c9d3c67efed0565975351f13a9
Elizabeth Wang <eyw3@illinois.edu>
2020-11-15 15:25:47 -0800

    Implement parsing to get link containing paper submissions

--- utils/build_data_from_web.py
+++ utils/build_data_from_web.py
@@ -18,7 +18,9 @@ class DataSetBuilder:
 
     def build_data_set(self, data_set_name):
         for conference_name in self.__conference_abbrevs:
-            self.__get_conference_events(conference_name)
+            events = self.__get_conference_events(conference_name)
+            content_urls = self.__get_content_urls(events)
+            print(content_urls)
 
     def __get_conference_events(self, conference_name):
         CONFERENCE_BASE_URL = "https://dblp.org/db/conf/"
@@ -28,3 +30,13 @@ class DataSetBuilder:
         data = urllib.request.urlopen(conference_url).read().decode("utf-8")
         # Get a list of all events for the current conference by following "cite" component
         return BeautifulSoup(data, 'html.parser').find_all('cite', {'class': 'data'})
+
+    def __get_content_urls(self, conference_events):
+        content_urls = []
+        for event in conference_events[ : self.__num_events_per_conference]:
+            content_urls.append(event.find_all('a', {'class': 'toc-link'}))
+        return content_urls
+
+if __name__ == "__main__":
+    data_set_builder = DataSetBuilder(2, ['aciids'])
+    data_set_builder.build_data_set("b")
