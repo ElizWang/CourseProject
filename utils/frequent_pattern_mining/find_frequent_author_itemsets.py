bae878d6a79ceacea2efd68e15f8fe938092f3c8
Elizabeth Wang <eyw3@illinois.edu>
2020-11-15 19:38:25 -0800

    Add pycache to gitignore

--- utils/frequent_pattern_mining/find_frequent_author_itemsets.py
+++ utils/frequent_pattern_mining/find_frequent_author_itemsets.py
@@ -1,10 +1,19 @@
 import spmf_python_wrapper
 import os
 
-# NOTE: smpf uses a relative threshold for support
+'''
+Generate a file (data/frequent_author_patterns.txt) containing all frequent author patterns via SMPF
+-- this file encodes authors as IDs for efficiency (according to SMPF, which also requires that all
+input files contain numbers rather than words). Generates a file containing the mappings between 
+author names and authors IDs, which we can use later to display our results.
+
+NOTE: Must run this file from the project's root directory for the paths to work
+'''
+
 CSV_FILE_PATH = "data/data.csv"
 INPUT_FILE_PATH = "data/temp_spmf_input.txt"
 OUTPUT_FILE_PATH = "data/frequent_author_patterns.txt"
+AUTHOR_ID_FILE_PATH = "data/author_id_mappings.txt"
 
 # TODO move elsewhere, build multiple files so we only have to traverse data.csv once
 def build_intermediate_smpf_input():
@@ -28,10 +37,18 @@ def build_intermediate_smpf_input():
 
         input_file.write("%s\n" % ' '.join(author_ids))
         paper_count += 1
-
+        
     csv_file.close()
     input_file.close()
     return author_id_mapping
 
-build_intermediate_smpf_input()
-spmf_python_wrapper.run_spmf("FPClose", INPUT_FILE_PATH, OUTPUT_FILE_PATH, ["10%"])
+def write_author_id_mapping(author_id_mapping):
+    author_id_file = open(AUTHOR_ID_FILE_PATH, "w")
+    for author in author_id_mapping:
+        author_id_file.write("%d %s\n" % (author_id_mapping[author], author))
+    author_id_file.close()
+
+if __name__ == "__main__":
+    author_id_mapping = build_intermediate_smpf_input()
+    write_author_id_mapping(author_id_mapping)
+    spmf_python_wrapper.run_spmf("FPClose", INPUT_FILE_PATH, OUTPUT_FILE_PATH, ["10%"])
