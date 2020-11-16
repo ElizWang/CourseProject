828c745ae898cc63dd4c6569045525337fdb3eae
Elizabeth Wang <eyw3@illinois.edu>
2020-11-15 19:01:21 -0800

    Add spmf.jar download

--- setup.sh
+++ setup.sh
@@ -10,3 +10,16 @@ fi
 
 echo "Building data.csv file from DBLP dataset"
 python utils/build_data_from_web.py
+
+if [ -d "libs" ]
+then
+  echo "libs/ directory exists. WARNING: May override existing spmf.jar file" 
+else
+  echo "libs/ directory doesn't exist. Creating directory now"
+  mkdir libs 
+fi
+
+echo "Downloading spmf.jar file to mine frequent patterns"
+cd libs
+wget http://www.philippe-fournier-viger.com/spmf/download-spmfjar.php -O spmf.jar
+cd ..
