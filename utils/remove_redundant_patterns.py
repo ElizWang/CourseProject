828e819f4303b52935fac1398bceb6969dd6d012
Elizabeth Wang <eyw3@illinois.edu>
2020-11-16 11:20:29 -0800

    Implement one pass microclustering setup

--- utils/remove_redundant_patterns.py
+++ utils/remove_redundant_patterns.py
@@ -9,3 +9,17 @@ def calculate_jaccard_distance(pattern_1, pattern_2):
     intersection_len = len(pattern_1_set.intersection(pattern_2_set))
     union_len = len(pattern_1_set.union(pattern_2_set))
     return 1 - intersection_len / union_len
+
+def find_one_pass_microclustering_patterns(patterns):
+    # Patterns are denoted by their index
+    # Jaccard dists are keyed via tuples -- (p1_ind, p2_ind) such that 
+    # p1_ind < p2_ind
+    jaccard_dists = {}
+    num_patterns = len(patterns)
+    for i in range(num_patterns):
+        for j in range(i, num_patterns):
+            jaccard_dists[ (i ,j) ] = calculate_jaccard_distance(patterns[i], patterns[j])
+
+from parse_patterns import parse_file_into_patterns
+patterns = parse_file_into_patterns("data/toy_title_patterns.txt")
+find_one_pass_microclustering_patterns(patterns)
