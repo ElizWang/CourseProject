194295be5d7459f537bfaea82eba681b8e71b13d
Elizabeth Wang <eyw3@illinois.edu>
2020-11-24 22:24:10 -0800

    Add title-title pattern support

--- pattern_annotators/representative_transaction_extractor.py
+++ pattern_annotators/representative_transaction_extractor.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", "utils"))
 
 from transactions_manager import TransactionsManager
 from mutual_information_manager import MutualInformationManager
-from parse_patterns import parse_author_file_into_patterns
+from parse_patterns import parse_author_file_into_patterns, parse_sequential_title_file_into_patterns
 from cosine_similarity import compute_cosine_similarity
 
 '''
@@ -30,12 +30,13 @@ class RepresentativeTransactionExtractor:
         def __eq__(self, other): 
             return self.cosine_sim == other.cosine_sim
 
-    def __init__(self, transaction_mananger, mutual_info_manager, patterns, num_transactions, is_author):
+    def __init__(self, transaction_mananger, mutual_info_manager, patterns, pattern_type, num_transactions, is_author):
         '''
         @param
             transaction_mananger: TransactionsManager       Object storing all transactions (every line from data.csv)
             mutual_info_manager: MutualInformationManager   Object storing a set of all author-author mutual information vals
             patterns: list(list(int))                       List of all SSP patterns
+            pattern_type: PatternType                       Type of pattern pairs to compute MI for
             num_transactions: int                           Top k most representative transactions to find
             is_author: bool                                 True if SSP == authors, false otherwise
         '''
@@ -43,6 +44,10 @@ class RepresentativeTransactionExtractor:
         self.__mutual_info_manager = mutual_info_manager
 
         self.__patterns = patterns
+        assert pattern_type == MutualInformationManager.PatternType.AUTHOR_AUTHOR \
+            or pattern_type == MutualInformationManager.PatternType.TITLE_TITLE
+        self.__pattern_type = pattern_type
+
         self.__num_transactions = num_transactions
         self.__is_author = is_author
 
@@ -57,7 +62,11 @@ class RepresentativeTransactionExtractor:
         @return list(int):
             k most semantically similar patterns, sorted in decreasing similarity
         '''
-        paper_context_models = self.__transaction_manager.compute_author_context_models(self.__patterns)
+        if self.__pattern_type == MutualInformationManager.PatternType.AUTHOR_AUTHOR:
+            paper_context_models = self.__transaction_manager.compute_author_context_models(self.__patterns)
+        elif self.__pattern_type == MutualInformationManager.PatternType.TITLE_TITLE:
+             paper_context_models = self.__transaction_manager.compute_title_context_models(self.__patterns)
+           
         context_model_dim = len(self.__patterns)
 
         similarities_max_q = []
@@ -85,15 +94,40 @@ class RepresentativeTransactionExtractor:
         top_transactions list(int):         list of all indices (aka ids) of the papers ordered from
             most representative to least representative and of size num_transactions
         '''
+        if self.__pattern_type == MutualInformationManager.PatternType.AUTHOR_AUTHOR:
+            self.__display_pretty_authors_pattern_title_transactions(pattern_id, top_transactions)
+        elif self.__pattern_type == MutualInformationManager.PatternType.TITLE_TITLE:
+            self.__display_pretty_title_pattern_transactions(pattern_id, top_transactions)
+
+    def __display_pretty_authors_pattern_title_transactions(self, pattern_id, top_transactions):
         pattern = self.__patterns[pattern_id]
+
         pattern_words = [self.__transaction_manager.get_author_name(word_id) for word_id in pattern]
         print("Input pattern: %s" % ' '.join(pattern_words))
+        self.__display_title_terms(top_transactions)
 
+    def __display_pretty_title_pattern_transactions(self, pattern_id, top_transactions):
+        pattern = self.__patterns[pattern_id]
+
+        pattern_words = [self.__transaction_manager.get_title_term(word_id) for word_id in pattern]
+        print("Input pattern: %s" % ' '.join(pattern_words))
+
+        self.__display_title_terms(top_transactions)
+
+        for transaction_ind in top_transactions:
+            transaction_auth_ids = self.__transaction_manager.get_paper_authors(transaction_ind)
+            transaction_auth_words = [self.__transaction_manager.get_author_name(word_id) \
+                for word_id in transaction_auth_ids]
+            print("Most representative title transactions %d: %s" % \
+                (transaction_ind, ' '.join(transaction_auth_words)))
+        print()
+
+    def __display_title_terms(self, top_transactions):
         for transaction_ind in top_transactions:
             transaction_title_ids = self.__transaction_manager.get_paper_title_terms(transaction_ind)
             transaction_title_words = [self.__transaction_manager.get_title_term(word_id) \
                 for word_id in transaction_title_ids]
-            print("Most representative transaction %d: %s" % \
+            print("Most representative title transactions %d: %s" % \
                 (transaction_ind, ' '.join(transaction_title_words)))
         print()
 
@@ -109,7 +143,14 @@ if __name__ == "__main__":
 
     transactions = TransactionsManager("data/data.csv", "data/author_id_mappings.txt", "data/title_term_id_mappings.txt")    
     author_patterns = parse_author_file_into_patterns("data/frequent_author_patterns.txt")
+    title_patterns = parse_sequential_title_file_into_patterns("data/minimal_title_term_patterns.txt")
     
-    extractor = RepresentativeTransactionExtractor(transactions, mutual_info, author_patterns, 3, True)
+    '''
+    extractor = RepresentativeTransactionExtractor(transactions, mutual_info, author_patterns, \
+        MutualInformationManager.PatternType.AUTHOR_AUTHOR, 3, True)
+    '''
+    extractor = RepresentativeTransactionExtractor(transactions, mutual_info, title_patterns, \
+        MutualInformationManager.PatternType.TITLE_TITLE, 3, True)
+    
     repr_transactions = extractor.find_representative_transactions(target_id, k)
     extractor.display_pretty(target_id, repr_transactions)
