24f586ff73c1be1d2ffe36cab8dd7926884652d7
Elizabeth Wang <eyw3@illinois.edu>
2020-11-25 10:39:23 -0800

    Annotate author patterns with author/title patterns and vica versa

--- pattern_annotators/strongest_context_indicator_extractor.py
+++ pattern_annotators/strongest_context_indicator_extractor.py
@@ -27,20 +27,31 @@ class StrongestContextIndicatorExtractor:
         def __eq__(self, other):
             return self.sim == other.sim
 
-    def __init__(self, mutual_info_manager, transaction_manager, patterns, pattern_type):
+    def __init__(self, mutual_info_manager, transaction_manager, patterns, pattern_type, alternate_patterns=None):
         '''
         @param
             transaction_mananger: TransactionsManager       Object storing all transactions (every line from data.csv)
             mutual_info_manager: MutualInformationManager   Object storing a set of all author-author mutual information vals
             patterns: list(list(int))                       List of all SSP patterns
             pattern_type: PatternType                       Type of pattern pairs to compute MI for
+            alternate_patterns list(list(int)):             If author-title or title-author, we pass in title/author patterns to
+                patterns and author/title patterns here
         '''
         self.__transaction_manager = transaction_manager
         self.__mutual_info_manager = mutual_info_manager
         self.__patterns = patterns
         assert pattern_type == MutualInformationManager.PatternType.AUTHOR_AUTHOR \
-            or pattern_type == MutualInformationManager.PatternType.TITLE_TITLE
+            or pattern_type == MutualInformationManager.PatternType.TITLE_TITLE \
+                or pattern_type == MutualInformationManager.PatternType.AUTHOR_TITLE \
+                    or pattern_type == MutualInformationManager.PatternType.TITLE_AUTHOR
+
         self.__pattern_type = pattern_type
+
+        if alternate_patterns:
+            assert pattern_type == MutualInformationManager.PatternType.AUTHOR_TITLE \
+                or pattern_type == MutualInformationManager.PatternType.TITLE_AUTHOR
+
+        self.__alternate_patterns = alternate_patterns
 
     def find_strongest_context_indicators(self, pattern_id, k):
         '''
@@ -69,20 +80,28 @@ class StrongestContextIndicatorExtractor:
         @param top_patterns list(int):
             ids of patterns to print out
         '''
-        pattern = self.__patterns[pattern_id]
+        if self.__alternate_patterns:
+            pattern = self.__alternate_patterns[pattern_id]
+        else:
+            pattern = self.__patterns[pattern_id]
 
-        if self.__pattern_type == MutualInformationManager.PatternType.AUTHOR_AUTHOR:
+        if self.__pattern_type == MutualInformationManager.PatternType.AUTHOR_AUTHOR \
+            or self.__pattern_type == MutualInformationManager.PatternType.AUTHOR_TITLE:
             pattern_words = [self.__transaction_manager.get_author_name(word_id) for word_id in pattern]
-        elif self.__pattern_type == MutualInformationManager.PatternType.TITLE_TITLE:
+
+        elif self.__pattern_type == MutualInformationManager.PatternType.TITLE_TITLE \
+            or self.__pattern_type == MutualInformationManager.PatternType.TITLE_AUTHOR:
             pattern_words = [self.__transaction_manager.get_title_term(word_id) for word_id in pattern]
 
         print("Input pattern: %s" % ' '.join(pattern_words))
         for pattern_id in top_patterns:
             top_pattern = self.__patterns[pattern_id]
 
-            if self.__pattern_type == MutualInformationManager.PatternType.AUTHOR_AUTHOR:
+            if self.__pattern_type == MutualInformationManager.PatternType.AUTHOR_AUTHOR \
+                or self.__pattern_type == MutualInformationManager.PatternType.TITLE_AUTHOR:
                 top_pattern_words = [self.__transaction_manager.get_author_name(word_id) for word_id in top_pattern]
-            elif self.__pattern_type == MutualInformationManager.PatternType.TITLE_TITLE:
+            elif self.__pattern_type == MutualInformationManager.PatternType.TITLE_TITLE \
+                or self.__pattern_type == MutualInformationManager.PatternType.AUTHOR_TITLE:
                 top_pattern_words = [self.__transaction_manager.get_title_term(word_id) for word_id in top_pattern]
             print("Pattern: %s" % ' '.join(top_pattern_words))
 
@@ -96,20 +115,43 @@ if __name__ == '__main__':
 
     transactions = TransactionsManager("data/data.csv", "data/author_id_mappings.txt", "data/title_term_id_mappings.txt")
 
+    author_patterns = parse_author_file_into_patterns("data/frequent_author_patterns.txt")
+    title_patterns = parse_sequential_title_file_into_patterns("data/minimal_title_term_patterns.txt")
+
     if is_auth_experiment:
+        # Annotate with author patterns
         mutual_info = MutualInformationManager(MutualInformationManager.PatternType.AUTHOR_AUTHOR)
         mutual_info.read_mutual_information_from_file()
 
-        author_patterns = parse_author_file_into_patterns("data/frequent_author_patterns.txt")
         extractor = StrongestContextIndicatorExtractor(mutual_info, transactions, author_patterns, \
             MutualInformationManager.PatternType.AUTHOR_AUTHOR)
+        strongest_indicators = extractor.find_strongest_context_indicators(target_id, k)
+        extractor.pretty_print(target_id, strongest_indicators)
+
+        # Annotate with title patterns
+        mutual_info = MutualInformationManager(MutualInformationManager.PatternType.AUTHOR_TITLE)
+        mutual_info.read_mutual_information_from_file()
+
+        extractor = StrongestContextIndicatorExtractor(mutual_info, transactions, title_patterns, \
+            MutualInformationManager.PatternType.AUTHOR_TITLE, author_patterns)
+        strongest_indicators = extractor.find_strongest_context_indicators(target_id, k)
+        extractor.pretty_print(target_id, strongest_indicators)
+
     else:
+        # Annotate with title patterns
         mutual_info = MutualInformationManager(MutualInformationManager.PatternType.TITLE_TITLE)
         mutual_info.read_mutual_information_from_file()
 
-        title_patterns = parse_sequential_title_file_into_patterns("data/minimal_title_term_patterns.txt")
         extractor = StrongestContextIndicatorExtractor(mutual_info, transactions, title_patterns, \
             MutualInformationManager.PatternType.TITLE_TITLE)
-    
-    strongest_indicators = extractor.find_strongest_context_indicators(target_id, k)
-    extractor.pretty_print(target_id, strongest_indicators)
+        strongest_indicators = extractor.find_strongest_context_indicators(target_id, k)
+        extractor.pretty_print(target_id, strongest_indicators)
+
+        # Annotate with author patterns
+        mutual_info = MutualInformationManager(MutualInformationManager.PatternType.TITLE_AUTHOR)
+        mutual_info.read_mutual_information_from_file()
+
+        extractor = StrongestContextIndicatorExtractor(mutual_info, transactions, author_patterns, \
+            MutualInformationManager.PatternType.TITLE_AUTHOR, title_patterns)
+        strongest_indicators = extractor.find_strongest_context_indicators(target_id, k)
+        extractor.pretty_print(target_id, strongest_indicators)
